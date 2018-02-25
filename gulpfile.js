6e95f2178bce8586e7448935f037b6a713dd5bd1
Elsa Brown <elsambrown@gmail.com>
2018-02-25 16:15:00 -0500

    rewrite gulpfile.js, add browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,34 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
 
-gulp.task('sass-build', () => {
+gulp.task('build', () => {
 	return gulp
-		.src('src/scss/*.scss')
+		.src('app/scss/**/*.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass({ outputStyle: 'expanded' })
 			.on('error', sass.logError))
 		.pipe(autoprefixer({ browsers: ['last 2 versions'] }))
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('./dist/css'))
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 });
 
-gulp.task('sass-watch', ['sass-build'], () => {
-	gulp.watch('sass/**/*.scss', ['sass-build'])
+gulp.task('browserSync', () => {
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		},
+	});
 });
 
-gulp.task('default', ['sass-watch', 'sass-build']);
+gulp.task('watch', ['browserSync', 'build'], () => {
+	gulp.watch('app/scss/**/*.scss', ['build']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*/js', browserSync.reload);
+});
+
+gulp.task('default', ['watch', 'browserSync', 'build']);
